f37eca1387303be31b5657ccf31abfeadab87731
re2osushi8888 <re.2osushi8888@gmail.com>
2023-11-15 23:02:46 +0900

    add:  test_金額に100を持つ円を作成できる

--- tests/src/test_yen.py
+++ tests/src/test_yen.py
@@ -5,3 +5,7 @@ class TestYen:
     def test_金額に10を持つ円を作成できる(self):
         yen = Yen(10)
         assert yen.amount == 10
+
+    def test_金額に100を持つ円を作成できる(self):
+        yen = Yen(100)
+        assert yen.amount == 100
