1fc25349b197c775bc03441e13729b198cfb30a6
re2osushi8888 <re.2osushi8888@gmail.com>
2023-11-15 22:44:56 +0900

    add: test_100円を投入できる

--- tests/src/test_controller.py
+++ tests/src/test_controller.py
@@ -11,3 +11,11 @@ class TestMoneyController:
         money = controller.input()
         expected = "10円投入されました"
         assert expected == money
+
+    def test_100円を投入できる(self, monkeypatch):
+        mock_inputs = StringIO('100\n')
+        monkeypatch.setattr('sys.stdin', mock_inputs)
+        controller = MoneyController()
+        money = controller.input()
+        expected = "100円投入されました"
+        assert expected == money
