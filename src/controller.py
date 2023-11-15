34664c73b93d057b9d8f8de73ec33bbab77de979
re2osushi8888 <re.2osushi8888@gmail.com>
2023-11-15 22:55:00 +0900

    add: inputにバリデーション作成

--- src/controller.py
+++ src/controller.py
@@ -2,5 +2,7 @@
 
 class MoneyController:
     def input(self):
-        money = input("お金を投入してください")
+        money = input('お金を投入してください')
+        if money not in ['10', '50', '100', '500', '1000']:
+            raise ValueError('硬貨・お札は1つずつ入れてください')
         return f'{money}円投入されました'
