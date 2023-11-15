74be40398661ca20ff953d5fe3f2e4d18395e683
re2osushi8888 <re.2osushi8888@gmail.com>
2023-11-15 23:07:25 +0900

    add: test_11円の場合ValueError

--- tests/src/test_yen.py
+++ tests/src/test_yen.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.yen import Yen
 
 
@@ -9,3 +11,8 @@ class TestYen:
     def test_金額に100を持つ円を作成できる(self):
         yen = Yen(100)
         assert yen.amount == 100
+
+    def test_11円の場合ValueError(self):
+        with pytest.raises(ValueError) as e:
+            Yen(11)
+        assert e.value == "通貨ではありません"
