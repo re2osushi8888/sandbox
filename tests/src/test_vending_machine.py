7e8627aeba5119c259b6930284e4632cd6fc0db0
re2osushi8888 <re.2osushi8888@gmail.com>
2023-11-18 18:07:54 +0900

    init: TestVendingMachine
    テストケースの作成のみ

---
+++ tests/src/test_vending_machine.py
@@ -0,0 +1,11 @@
+
+class TestVendingMachine:
+    # def test_円を1つずつ投入できる():
+
+    # def test_通貨にない数字を入力すると投入やり直し():
+
+    # def test_お金の投入を複数回できる():
+
+    # def test_投入金額の総計を取得できる():
+
+    # def test_払い戻し操作を行うと投入金額の総計を釣銭として出力する():
