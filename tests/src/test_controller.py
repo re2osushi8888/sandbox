9ced3ac6393c7b7e99b4b838c846648dd720f869
re2osushi8888 <re.2osushi8888@gmail.com>
2023-12-02 17:23:10 +0900

    change: MoneyControllerの仕様を変更するためにテストを変更

--- tests/src/test_controller.py
+++ tests/src/test_controller.py
@@ -3,30 +3,31 @@ from io import StringIO
 import pytest
 
 from src.controller import MoneyController
+from src.yen import Yen
 
 
 class TestMoneyController:
-    def test_10円を投入できる(self, monkeypatch):
+    def test_10を入力したら10円が返ってくる(self, monkeypatch):
         mock_inputs = StringIO('10\n')
         monkeypatch.setattr('sys.stdin', mock_inputs)
         controller = MoneyController()
-        money = controller.input()
-        expected = "10円投入されました"
+        money: Yen = controller.input()
+        expected = Yen(10)
         assert expected == money
 
-    def test_100円を投入できる(self, monkeypatch):
-        mock_inputs = StringIO('100\n')
-        monkeypatch.setattr('sys.stdin', mock_inputs)
-        controller = MoneyController()
-        money = controller.input()
-        expected = "100円投入されました"
-        assert expected == money
+    # def test_100円を投入できる(self, monkeypatch):
+    #     mock_inputs = StringIO('100\n')
+    #     monkeypatch.setattr('sys.stdin', mock_inputs)
+    #     controller = MoneyController()
+    #     money = controller.input()
+    #     expected = "100円投入されました"
+    #     assert expected == money
 
-    def test_11円を投入したらValueError(self, monkeypatch):
-        mock_inputs = StringIO('11\n')
-        monkeypatch.setattr('sys.stdin', mock_inputs)
-        controller = MoneyController()
-        with pytest.raises(ValueError) as e:
-            controller.input()
-        expected = "硬貨・お札は1つずつ入れてください"
-        assert str(e.value) == expected
+    # def test_11円を投入したらValueError(self, monkeypatch):
+    #     mock_inputs = StringIO('11\n')
+    #     monkeypatch.setattr('sys.stdin', mock_inputs)
+    #     controller = MoneyController()
+    #     with pytest.raises(ValueError) as e:
+    #         controller.input()
+    #     expected = "硬貨・お札は1つずつ入れてください"
+    #     assert str(e.value) == expected
