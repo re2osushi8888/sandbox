86a5b182389b3115cbdd680c93e5ea9a0b697674
re2osushi8888 <re.2osushi8888@gmail.com>
2023-12-02 17:29:04 +0900

    add: test_100と入力したら100円が返返ってくる

--- tests/src/test_controller.py
+++ tests/src/test_controller.py
@@ -14,13 +14,12 @@ class TestMoneyController:
         money: Yen = controller.input()
         assert Yen(10).amount == money.amount
 
-    # def test_100円を投入できる(self, monkeypatch):
-    #     mock_inputs = StringIO('100\n')
-    #     monkeypatch.setattr('sys.stdin', mock_inputs)
-    #     controller = MoneyController()
-    #     money = controller.input()
-    #     expected = "100円投入されました"
-    #     assert expected == money
+    def test_100と入力したら100円が返返ってくる(self, monkeypatch):
+        mock_inputs = StringIO('100\n')
+        monkeypatch.setattr('sys.stdin', mock_inputs)
+        controller = MoneyController()
+        money: Yen = controller.input()
+        assert Yen(100).amount == money.amount
 
     # def test_11円を投入したらValueError(self, monkeypatch):
     #     mock_inputs = StringIO('11\n')
