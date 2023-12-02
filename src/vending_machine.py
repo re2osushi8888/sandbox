66d2809fbf1f995deee242e4a7f7720340f127a8
re2osushi8888 <re.2osushi8888@gmail.com>
2023-12-03 00:54:14 +0900

    add: VendingMachineにtotal_amount追加、input_yenに入力されたyenをtotal_amountに保存

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -1,9 +1,14 @@
+from typing import List
+
 from src.yen import Yen
 
 
 class VendingMachine:
+    def __init__(self):
+        self.total_amount: List(Yen) = []
 
     def input_yen(self, yen: Yen) -> str:
+        self.total_amount.append(yen)
         return f'{yen.amount}円が投入されました'
 
     def fetch_total_amount(self) -> int:
