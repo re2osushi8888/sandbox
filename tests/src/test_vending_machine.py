7239c7b1d24d72c282552e06bc9781a631f3c0e9
re2osushi8888 <re.2osushi8888@gmail.com>
2023-12-02 17:33:26 +0900

    add: test_500円と1000円を投入し投入金額の総計を取得できる()

--- tests/src/test_vending_machine.py
+++ tests/src/test_vending_machine.py
@@ -13,10 +13,17 @@ class TestVendingMachine:
         message = machine.input_yen(Yen(100))
         assert message == '100円が投入されました'
 
+    def test_500円と1000円を投入し投入金額の総計を取得できる():
+        machine = VendingMachine()
+        machine.input_yen(Yen(500))
+        machine.input_yen(Yen(1000))
+        total_amount: Yen = machine.fetch_total_amount()
+        excpected = Yen(1500)
+        assert total_amount.amount == excpected.amount
+
+
     # def test_通貨にない数字を入力すると投入やり直し():
 
     # def test_お金の投入を複数回できる():
-
-    # def test_投入金額の総計を取得できる():
 
     # def test_払い戻し操作を行うと投入金額の総計を釣銭として出力する():
