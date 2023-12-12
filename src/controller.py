7b000a4d7d530960bc82a6071ff946e13219970b
re2osushi8888 <re.2osushi8888@gmail.com>
2023-12-12 23:02:22 +0900

    delete: MoneyController削除

--- src/controller.py
+++ src/controller.py
@@ -13,12 +13,3 @@ class CliController:
             return num
         except ValueError:
             raise ValueError('数値で入力してください')
-
-
-class MoneyController:
-    def input(self):
-        money_amount = input('お金を投入してください')
-        if money_amount not in ['10', '50', '100', '500', '1000']:
-            raise ValueError('硬貨・お札は1つずつ入れてください')
-        money = Yen(int(money_amount))
-        return money
--- tests/src/test_controller.py
+++ tests/src/test_controller.py
@@ -2,8 +2,7 @@ from io import StringIO
 
 import pytest
 
-from src.controller import CliController, MoneyController
-from src.yen import Yen
+from src.controller import CliController
 
 
 class TestCliController:
@@ -31,29 +30,3 @@ class TestCliController:
         with pytest.raises(ValueError) as e:
             controller.input_str_to_int()
         assert str(e.value) == "数値で入力してください"
-
-
-class TestMoneyController:
-    def test_10と入力したら10円が返ってくる(self, monkeypatch):
-        mock_inputs = StringIO('10\n')
-        monkeypatch.setattr('sys.stdin', mock_inputs)
-        controller = MoneyController()
-        money: Yen = controller.input()
-        assert Yen(10).amount == money.amount
-
-    def test_100と入力したら100円が返返ってくる(self, monkeypatch):
-        mock_inputs = StringIO('100\n')
-        monkeypatch.setattr('sys.stdin', mock_inputs)
-        controller = MoneyController()
-        money: Yen = controller.input()
-        assert Yen(100).amount == money.amount
-
-    def test_11と入力したらValueError(self, monkeypatch):
-        mock_inputs = StringIO('11\n')
-        monkeypatch.setattr('sys.stdin', mock_inputs)
-        controller = MoneyController()
-        with pytest.raises(ValueError) as e:
-            controller.input()
-        expected = "硬貨・お札は1つずつ入れてください"
-        assert str(e.value) == expected
-
