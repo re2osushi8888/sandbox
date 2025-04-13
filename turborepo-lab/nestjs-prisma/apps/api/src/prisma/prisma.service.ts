f178e4c1c3e452b125cada36ef485e540238c742
re2osushi8888 <re.2osushi8888@gmail.com>
2025-04-13 11:32:53 +0000

    警告を一時的に無視

--- turborepo-lab/nestjs-prisma/apps/api/src/prisma/prisma.service.ts
+++ turborepo-lab/nestjs-prisma/apps/api/src/prisma/prisma.service.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '@repo/database';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
   }
 }
