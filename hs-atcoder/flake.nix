f3bb1abf4bfd786f2afe363055721491c661b625
re2osushi8888 <re.2osushi8888@gmail.com>
2026-05-01 21:33:10 +0900

    atcoder-cliコマンドをインストール

--- hs-atcoder/flake.nix
+++ hs-atcoder/flake.nix
@@ -13,6 +13,18 @@
         hpkgs = pkgs.haskellPackages;
         fmt = pkgs.writeShellScriptBin "fmt" "fourmolu --mode inplace \"$@\"";
         run = pkgs.writeShellScriptBin "run" "runghc \"$@\"";
+        atcoder-cli = pkgs.buildNpmPackage {
+          pname = "atcoder-cli";
+          version = "2.2.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "Tatamo";
+            repo  = "atcoder-cli";
+            rev   = "v2.2.0";
+            hash  = "sha256-7pbCTgWt+khKVyMV03HanvuOX2uAC0PL9OLmqly7IWE=";
+          };
+          npmDepsHash = "sha256-ufG7Fq5D2SOzUp8KYRYUB5tYJYoADuhK+2zDfG0a3ks=";
+          NODE_OPTIONS = "--openssl-legacy-provider";
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = [
@@ -21,6 +33,7 @@
             hpkgs.fourmolu
             fmt
             run
+            atcoder-cli
           ];
         };
       });
