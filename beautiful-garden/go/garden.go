de182a0ac6556bae16f1ca6bbf77f46966662618
re2osushi8888 <re.2osushi8888@gmail.com>
2026-06-30 00:32:06 +0900

    LHHとHLLのパターンのテストを作成

--- beautiful-garden/go/garden.go
+++ beautiful-garden/go/garden.go
@@ -1,5 +1,21 @@
 package main
 
 func minCuts(x int, y int, z int) (int, int, int) {
-	return 5, 4, 3
+	// H -> L
+	if x > y {
+		// H -> L -> L
+		if y > z {
+			a := y - 1
+		return a, y, z
+		}
+	}
+	// L -> H
+	if x < y {
+		// L -> H -> H
+		if y < z {
+			c := y - 1
+			return x,y,c
+		}
+	}
+	return x, y, z
 }
--- beautiful-garden/go/garden_test.go
+++ beautiful-garden/go/garden_test.go
@@ -3,9 +3,18 @@ package main
 import "testing"
 
 func TestMinCuts(t *testing.T) {
-	x, y, z := 3, 4, 5
+	x, y, z := 5,4,3  
 	a, b, c := minCuts(x, y, z)
-	wantA, wantB, wantC := 5, 4, 3
+	wantA, wantB, wantC := 3, 4, 3
+	if a != wantA || b != wantB || c != wantC {
+		t.Errorf("minCuts(%d, %d, %d) = (%d, %d, %d), want (%d, %d, %d)",
+			x, y, z, a, b, c, wantA, wantB, wantC)
+	}
+}
+func TestMinCuts2(t *testing.T) {
+	x, y, z := 3 , 4, 5 
+	a, b, c := minCuts(x, y, z)
+	wantA, wantB, wantC := 3, 4, 3
 	if a != wantA || b != wantB || c != wantC {
 		t.Errorf("minCuts(%d, %d, %d) = (%d, %d, %d), want (%d, %d, %d)",
 			x, y, z, a, b, c, wantA, wantB, wantC)
